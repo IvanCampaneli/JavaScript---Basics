6b242d4efb3aa4b5ac7a0042989a0537824d5abb
Ivan Campaneli <ivan.campaneli@hotmail.com>
2023-03-27 16:47:09 -0300

    specifications.js

---
+++ Seção 2 - JavaScript (básico)/specifications.js
@@ -0,0 +1,9 @@
+// String, number, undefined, null, boolean, symbol
+// NaN - Not a number
+// typeof - verifica o tipo de elemento utilizado no código.
+
+const name = "Ivan"; //string
+const num = 28; //number
+let nomeMateria; //undefined -> não aponta para local nenhum na memória
+const sobrenome = null; //null -> não aponta para local nenhum na memória
+const certificado = true; //Boolean = true, false (lógico)
