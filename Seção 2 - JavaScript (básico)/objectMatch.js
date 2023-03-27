814d570b8f6af0d44c736b9be172c08e5359593b
Ivan Campaneli <ivan.campaneli@hotmail.com>
2023-03-27 16:45:50 -0300

    objectMatch.js

---
+++ Seção 2 - JavaScript (básico)/objectMatch.js
@@ -0,0 +1,13 @@
+let num1 = 9.49;;
+
+//let num2= Math.floor(num1);// arredonda o número para baixo
+//let num2= Math.ceil(num1);// arredonda o número para cima
+//let num2= Math.round(num1);// arredonda o número para o mais próximo. ex: 9.49= 9 | 9.50 ou 9.51 = arredonda para cima | ou seja, da metade para cima se arredondo para cima
+//console.log(Math.max(1, 2, 3, 5, -10, 10000, 9, 80));; // captura o maior número da sequência
+//console.log(Math.min(1, 2, 3, 5, -10, 10000, 9, 80));; // captura o menor número da sequência
+//const aleatorio = Math.round(Math.random() * (10 - 5) + 5);; gera um número inteiro aleatório entre 10 e 5;;
+
+//console.log(Math.pow(2, 10)); //elevar número | ou console.log(2 ** 10); gera o mesmo resultado//
+//console.log(num1 ** 0.5); //descobrir a raiz quadrada
+//Js: Infinity = true
+
